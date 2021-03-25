4134a8b84e81ed5f1b7d6bfcf18b21f6c063daf7
Investiga Dus <investigadus@senescyt.gob.ec>
2021-03-24 21:20:16 -0500

    deps: upgrade decidim deps

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "10.0.99.52", user: "ruby-data", roles: %w(app db web worker)
+server "localhost", user: "ruby-data", roles: %w(app db web worker)
 set branch: "master"
 set :rails_env, "production"
